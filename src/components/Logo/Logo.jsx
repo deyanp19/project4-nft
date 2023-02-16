15909c44847bb492bb8e2b886d1ab6e4eba21f7b
deyan petrov <deyanp19@hotmail.com>
2023-02-16 13:32:10 +0200

    task 2

---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,18 @@
+function Logo(props) {
+
+    if (props.type==='default') {
+        return (
+            <img src='/images/logo.svg'  alt='logo' />
+         );
+    } else if(props.type==='muted') {
+        <img src='/images/logo-muted.svg' alt='logo muted' />
+    }
+     
+   
+}
+
+Logo.defaultProps={
+    type:'default'
+};
+
+export default Logo;
