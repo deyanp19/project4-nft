cead903e36677f5e3faaeee1b83489df0a57dc9f
deyan petrov <deyanp19@hotmail.com>
2023-02-16 14:15:42 +0200

    go

--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -1,11 +1,11 @@
 
-function Logo(props) {
-console.log(props);
-    if (props.type==='default') {
+export default function Logo({type='default'}) {
+ 
+    if (type==='default') {
         return (
             <img src='/images/logo.svg'  alt='logo' />
          );
-    } else if (props.type==='muted') {
+    } else if (type==='muted') {
         return (
             <img src='/images/logo-muted.svg' alt='logo muted' />
             )
@@ -18,4 +18,4 @@ Logo.defaultProps={
     type:'default'
 };
 
-export default Logo;
+ 
