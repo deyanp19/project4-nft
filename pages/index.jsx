323a2c12e9c48cedd205149d21368f30dcdaeb01
deyan petrov <deyanp19@hotmail.com>
2023-02-20 19:11:46 +0200

    avatar6

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,7 @@
 import { Container } from "@mui/material";
  
 import Header from "../src/components/header/Header";
-import User from "../src/components/user/User";
+// import User from "../src/components/user/User";
  
  
 export default function Index() {
