6fe76a4e3489e585fe664017fe2b47f6f95af93e
deyan petrov <deyanp19@hotmail.com>
2023-02-23 21:57:08 +0200

    footer fex

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,7 +2,7 @@ import { Container } from "@mui/material";
 import Auctions from "../src/components/auctions/Auctions";
 import Card from "../src/components/card/Card";
  import Example from '../src/components/example/Example'
-import Footer from "../src/components/footer/footer";
+import Footer from "../src/components/footer/Footer";
 import Header from "../src/components/header/Header";
 import Trending from "../src/components/trending/Trending";
 import User from "../src/components/user/User";
