044088ac25eb1043f03bba5b337a5650cf3a2dd9
deyan petrov <deyanp19@hotmail.com>
2023-02-28 16:25:22 +0200

    image

--- src/components/product/ProductImage.jsx
+++ src/components/product/ProductImage.jsx
@@ -6,7 +6,7 @@ import styles from './ProductImage.module.scss';
 export default function ProductImage({url}) {
     console.log(url);
     return (
-        <div  className={classNames("product-image")} >
+        <div  className={classNames(styles.productImage)} >
             
                 <img src={url} className={classNames(styles.image)} />
               
