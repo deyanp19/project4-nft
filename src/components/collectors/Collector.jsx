7157d2fb7735a2a3ae88a467210a43c4d209bd47
deyan petrov <deyanp19@hotmail.com>
2023-02-28 09:01:35 +0200

    collector

--- src/components/collectors/Collector.jsx
+++ src/components/collectors/Collector.jsx
@@ -4,7 +4,7 @@ import User from '../user/User';
 export default function Collector({name,avatar,verified=true,type='darker',nftsCount}) {
     return (
         <div>
-            <User name={name} info={nftsCount} verified={verified} />
+            <User name={name} info={nftsCount} verified={verified} type={type} />
         </div>
     );
 }
--- src/components/collectors/CollectorColumn.jsx
+++ src/components/collectors/CollectorColumn.jsx
@@ -1,3 +1,5 @@
+import { Grid } from '@mui/material';
+import classNames from 'classnames';
 import Collector from './Collector';
 import styles from './Collector.module.scss';
 
@@ -12,13 +14,47 @@ export default function CollectorColumn({items=[]}) {
         "avatar": "/images/nft.jpg",
         "verified": Boolean,
         "id": 33
+      },
+      {
+        "name": 'jhon',
+        "nftsCount": "Number 3",
+        "avatar": "/images/nft.jpg",
+        "verified": Boolean,
+        "id": 33
       }
     ]
 
 console.log({items});
+
+    let light= styles.lighter;
+    let type;
     return (
-        <div>
-          {items.map((item)=> <Collector {...item}/>) }
-        </div>
+       items.map((item,i)=>{
+          if ((i)%2===0) {
+            light =styles.lighter;
+            type ='lighter'
+          } else{
+            light===styles.darker;
+            type='darker'
+          }
+
+          return (
+            <div className={light}>
+              <Grid key={i+item.name} className={styles.collectors} container spacing={2}>
+                <Grid item className={classNames(styles.collector)} >
+                  {i+1}
+                </Grid>
+                <Grid item >
+                  <Collector {...item} type={type}></Collector>
+                </Grid>
+              </Grid>
+            </div>
+
+          );
+
+      }) 
+
+
     );
+
 }
