448a515bc05175a0ef65488b23e5b3a991495942
deyan petrov <deyanp19@hotmail.com>
2023-02-28 18:22:35 +0200

    done2

--- src/components/product/ProductInfoTitle.jsx
+++ src/components/product/ProductInfoTitle.jsx
@@ -6,7 +6,7 @@ export default function ProductInfoTitle({text}) {
   return (
     <div className={classNames(styles["product-info-title"])}>
 
-        <Typography className={classNames(styles.title)} variant="h3" display="block" gutterBottom >
+        <Typography className={classNames(styles.title)} variant="h3"  gutterBottom >
             {text}
         </Typography>
     </div>
