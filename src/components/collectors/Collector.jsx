0108b275396bbfa2bafcf3a01b63b1cd2ba09101
deyan petrov <deyanp19@hotmail.com>
2023-02-28 09:39:13 +0200

    collector

--- src/components/collectors/Collector.jsx
+++ src/components/collectors/Collector.jsx
@@ -3,8 +3,8 @@ import User from '../user/User';
 
 export default function Collector({name,avatar,verified=true,type='darker',nftsCount}) {
     return (
-        <div>
-            <User name={name} info={nftsCount} verified={verified} type={type} />
+        <div className={styles.container}>
+            <User name={name} info={nftsCount} avatar={avatar} verified={verified} type={type} />
         </div>
     );
 }
--- src/components/collectors/CollectorColumn.jsx
+++ src/components/collectors/CollectorColumn.jsx
@@ -1,7 +1,7 @@
 import { Grid } from '@mui/material';
 import classNames from 'classnames';
 import Collector from './Collector';
-import styles from './Collector.module.scss';
+import styles from './CollectorColumn.module.scss';
 
   
 
@@ -32,19 +32,22 @@ console.log({items});
        items.map((item,i)=>{
           if ((i)%2===0) {
             light =styles.lighter;
-            type ='lighter'
+            type ='lighter';
+            console.log(type);
+
           } else{
             light===styles.darker;
             type='darker'
+            console.log(type);
           }
 
           return (
             <div className={light}>
               <Grid key={i+item.name} className={styles.collectors} container spacing={2}>
-                <Grid item className={classNames(styles.collector)} >
+                <Grid item className={classNames(styles.collector)} xs={1}>
                   {i+1}
                 </Grid>
-                <Grid item >
+                <Grid item  className={styles.collectors} xs={3}>
                   <Collector {...item} type={type}></Collector>
                 </Grid>
               </Grid>
