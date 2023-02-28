e8aca4e0677cbf6daf3b766feece54aa5626b612
deyan petrov <deyanp19@hotmail.com>
2023-02-28 17:22:52 +0200

    title

--- src/components/product/ProductInfoTitle.jsx
+++ src/components/product/ProductInfoTitle.jsx
@@ -2,7 +2,10 @@ import { Typography } from '@mui/material';
 import styles from './ProductInfoTitle.module.scss';
 
 export default function ProductInfoTitle({text}) {
-    <Typography className={classNames(styles["product-info-title"])}>
+    <div className={classNames(styles["product-info-title"])}>
+
+    <Typography >
         {text}
     </Typography>
+    </div>
 }
