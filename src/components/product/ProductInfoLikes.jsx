11edaf336009ec812a0ab9472186f912ee2b1d27
deyan petrov <deyanp19@hotmail.com>
2023-03-01 10:20:59 +0200

    likes no white spaces

--- src/components/product/ProductInfoLikes.jsx
+++ src/components/product/ProductInfoLikes.jsx
@@ -6,20 +6,6 @@ import classNames from 'classnames';
 
 
 export default function ProductInfoLikes({amount=0}) {
-    return (
-        <div className="product-info-likes" >
-                 
-                 
-                    <Chip className="likes"
-                        sx={{ borderRadius: "7px"}}
-                        color='secondary'
-                        icon={<FavoriteIcon fontSize='0.8125rem' />} 
-                        label={
-                            amount>0 ? (millify(likes,{ units: ["", "KB", "MB", "GB", "TB"]})):0} 
-                            
-                            variant="outlined" 
-                            />
-                 
-        </div>
-    );    
+    return ( <div className={classNames(styles["product-info-likes"])} >
+            <Chip className={classNames(styles.likes)} sx={{ borderRadius: "7px"}} color='secondary' icon={<FavoriteIcon fontSize='0.8125rem' />} label={amount>0 ? (millify(likes,{ units: ["", "KB", "MB", "GB", "TB"]})):0} variant="outlined" /> </div> );    
 }
