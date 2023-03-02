aa80deadb1af4eef60a4df4cbe41cf65c7ddf778
deyan petrov <deyanp19@hotmail.com>
2023-03-02 22:35:15 +0200

    how page

---
+++ pages/how/index.jsx
@@ -0,0 +1,37 @@
+import * as React from "react";
+import {useEffect, useState} from 'react';
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
+import Header from "../../src/components/header/Header";
+import Footer from "../../src/components/footer/Footer";
+import dataNtfs from "../../data/nfts.json";
+import { useRouter } from 'next/router';
+import Hero from "../../src/components/hero/Hero";
+import Description from "../../src/components/description/Description";
+
+
+
+
+export default function How(props) {
+    
+    const [ntfs,setNtfs]=useState();
+    useEffect(()=>{
+        setNtfs(dataNtfs)
+    },[]);
+
+    const router=useRouter();
+    
+
+  return (
+    <div>
+    <Container >
+      <Header />
+        <Hero text='fredrikd+a' />
+        <Description text="What are they they are the broken promise and to do is out of reach. Hand oer the candy notes food is so expensive to the Cashier." />
+       
+      <Footer />
+    </Container>
+      </div>
+  );
+}
