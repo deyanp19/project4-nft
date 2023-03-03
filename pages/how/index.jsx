70e4375a87c2c4c71e1431126a8b7e66856d1f16
deyan petrov <deyanp19@hotmail.com>
2023-03-03 12:38:59 +0200

    filters

--- pages/how/index.jsx
+++ pages/how/index.jsx
@@ -11,6 +11,7 @@ import Hero from "../../src/components/hero/Hero";
 import Description from "../../src/components/description/Description";
 import ProfileHero from "../../src/components/profile/ProfileHero";
 import ProfileUser from "../../src/components/profile/ProfileUser";
+import ProfileCollectionFilters from "../../src/components/profile/ProfileCollectionFilters";
 
 
 
@@ -33,7 +34,7 @@ export default function How(props) {
       <ProfileUser name='aladin' info='malko teksj' verified={false} />
         <Hero text='fredrikd+a' />
         <Description text="What are they they are the broken promise and to do is out of reach. Hand oer the candy notes food is so expensive to the Cashier." />
-    
+        <ProfileCollectionFilters ></ProfileCollectionFilters>
        
       <Footer />
     </Container>
--- src/components/profile/ProfileCollectionFilters.jsx
+++ src/components/profile/ProfileCollectionFilters.jsx
@@ -1,9 +1,82 @@
-import styles from './ProfileCollectionFilters.scss';
+import { Grid, Typography,InputAdornment,Stack,Select, TextField, FormControl, InputLabel, MenuItem } from '@mui/material';
+import {Search} from '@mui/icons-material';
+import classNames from 'classnames';
+import styles from './ProfileCollectionFilters.module.scss';
 
-export default function ProfileCollectionFilters({filters}) {
+
+
+export default function ProfileCollectionFilters({filters={
+    sort: [
+      { label: "Name (Ascending)", value: 1 },
+      { label: "Name (Descending)", value: 2 },
+    ],
+    price: [
+      { label: "0.3 - 0.5 ETH", value: 3 },
+      {
+        label: "0.5 - 2 ETH",
+        value: 4,
+      },
+      {
+        label: "2- 3 ETH",
+        value: 5,
+      },
+    ],
+    }}) {
+    console.log(filters);
     return (
         <div className={classNames(styles["profile-colleciton-filters"])}>
-            
+            <Grid container direction="row" sx={12} justifyContent="space-between" alignItems="center">
+                <Grid item >
+                    <Typography variant='h3' inputProps={{textTransform:'uppercase'}}>Collection</Typography>
+                </Grid>
+                <Grid item >
+                    <Grid container justifyContent="flex-end" spacing={2} >
+                        <Grid item >
+                            <FormControl >
+                                <InputLabel id="sort-order">Sort by</InputLabel>
+
+                                <Select 
+                                labelId="sort-order"
+                                id="sort-order"
+                                value={filters?.sort}
+                                label={filters.sort.label}
+                                >
+                                    {filters.sort.map((pumba)=><MenuItem value={pumba.value}>{pumba.label}</MenuItem>)}
+                                </Select>
+                            </FormControl>
+                            <FormControl  sx={{ m: 1}} size="small" >
+                                <Select 
+                                value="" //needs to be here because without value throws Error
+                                color='secondary'
+                                label='This week'
+                                className={classNames(styles.selectBox)} 
+                                >
+                                   {filters.price.map((timon)=><MenuItem value={timon.value}>{timon.label}</MenuItem>)}
+
+                                
+                                {/* </TextField> */}
+                                </Select>
+                    </FormControl>
+                            <FormControl >
+                                <TextField
+                                InputProps={{
+                                    startAdornment: (
+                                        <InputAdornment position='start' >
+
+                                        <Search sx={{color:'white'}}  />
+                                        </InputAdornment>
+                                    ),
+                                    disableUnderline:true
+                                }}
+                                className={classNames(styles.backgroundInputSearch)} 
+                                fullWidth 
+                                variant="standard" 
+                                color='secondary'  />
+                            </FormControl>
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Grid>
         </div>
     );
 }
