bc7e7f66baaa80b97207225afceefffa4ff10f84
deyan petrov <deyanp19@hotmail.com>
2023-03-03 16:08:33 +0200

    page profile done

---
+++ pages/profile/[id].jsx
@@ -0,0 +1,33 @@
+import Header from "../../src/components/header/Header";
+import ProfileHero from "../../src/components/profile/ProfileHero";
+import Footer from "../../src/components/footer/Footer";
+import { Container } from "@mui/material";
+import ProfileUser from "../../src/components/profile/ProfileUser";
+import ProfileCollection from "../../src/components/profile/ProfileCollection";
+import dataProfile from '../../data/profile.json';
+import { useEffect, useState } from "react";
+
+
+export default function Profile() {
+
+    const [profile, setProfile]=useState('');
+
+    useEffect(()=>{
+        setProfile(dataProfile);
+    },[]);
+
+    return (
+        <div>
+        <Container >
+          <Header />
+         
+            <ProfileHero />
+            <ProfileUser/>
+            <ProfileCollection />
+            
+           
+          <Footer />
+        </Container>
+          </div>
+    );
+}
