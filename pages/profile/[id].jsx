9a30210f02335247d2abec5d7cc9eefc8933d5fe
deyan petrov <deyanp19@hotmail.com>
2023-03-04 22:48:58 +0200

    opa

--- pages/profile/[id].jsx
+++ pages/profile/[id].jsx
@@ -7,6 +7,7 @@ import ProfileCollection from "../../src/components/profile/ProfileCollection";
 import dataProfile from '../../data/profile.json';
 import { useEffect, useState } from "react";
 import ActivityFilters from "../../src/components/activity/ActivityFilters";
+import ActivityListItem from "../../src/components/activity/ActivityListItem";
  
 
 
@@ -28,6 +29,7 @@ export default function Profile() {
            
             <ProfileCollection /> */}
             <ActivityFilters />
+            <ActivityListItem />
            
           <Footer />
         </Container>
--- src/components/activity/ActivityListItem.jsx
+++ src/components/activity/ActivityListItem.jsx
@@ -1,27 +1,29 @@
-import { Grid } from '@mui/material';
+import { Grid, Typography } from '@mui/material';
 // import { formatDistance, parseISO } from 'date-fns';
 import Avatar from '../avatar/Avatar';
 import Link from '../link/Link';
 import styles from './ActivityListItem.module.scss';
 import { format, formatDistance,parseISO, formatRelative, subDays } from 'date-fns';
 
-export default function ActivityListItem({user='', created_at='',nft='',type="like"}) {
+export default function ActivityListItem({user='flava', created_at='+02014101',nft='dicksmart',type="like"}) {
    
     
     return (
 
         
         <div className={styles.activity} >
-            <Grid container direction="row" >
+            <Grid container wrap="nowrap"  alignItems="center" justifyContent="flex-start" >
                 <Grid item >
-                    <Avatar  />
+                    <Avatar size={56} />
                 </Grid>
+            <Grid container direction="column">
                 <Grid item >
-                  {nft} {type}  <Link href='/' >{user}</Link>> 
+                  {user} {type}  <Link href='/' >{user}</Link>
                 </Grid>
             
            
-            { formatDistance(new Date()-3, new Date(parseISO(created_at, { additionalDigits: 1 })), {includeSeconds: true,addSuffix: true})}
+                <Typography> { formatDistance(new Date()-3, new Date(parseISO('+02014101', { additionalDigits: 1 })), {includeSeconds: true,addSuffix: true})}</Typography>
+            </Grid>
             </Grid>
         </div>
     );
