e998ce44dd0ebb838f21f85dcf38c5077fcf4fa7
deyan petrov <deyanp19@hotmail.com>
2023-03-04 21:57:34 +0200

    here activity

--- src/components/activity/ActivityFilters.jsx
+++ src/components/activity/ActivityFilters.jsx
@@ -51,25 +51,10 @@ export default function ActivityFilters({filters={
                                 className={classNames(styles.selectBox)} 
                                 >
                                    {filters.type.map((timon)=><MenuItem key={timon.value} value={timon.value}>{timon.label}</MenuItem>)}  
-
-                                
-                               {/* </TextField> */}
                                </Select>
                               </FormControl>
                               <FormControl sx={{padding:"4"}} >  
-                                <TextField variant="standard"
-                                inputprops={{
-                                    startAdornment: (
-                                        <InputAdornment position='start' >
-
-                                        <Search sx={{color:'white'}}  />
-                                        </InputAdornment>
-                                    ),
-                                    disableUnderline:true
-                                }}
-                                className={classNames(styles.backgroundInputSearch)} 
-                                fullWidth 
-                                color='secondary'  />
+                                <TextField variant="standard" InputProps={{  startAdornment: (  <InputAdornment position='start' > <Search sx={{color:'white'}}  /> </InputAdornment> ),   disableUnderline:true}} className={classNames(styles.backgroundInputSearch)}  fullWidth    color='secondary'  />
                             </FormControl>  
                         
                 </Grid>
