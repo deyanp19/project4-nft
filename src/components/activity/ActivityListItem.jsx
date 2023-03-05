cd0be8733e1d79c18df326a5a0cbb5e31a544e8c
deyan petrov <deyanp19@hotmail.com>
2023-03-05 17:05:10 +0200

    new push

--- src/components/activity/ActivityListItem.jsx
+++ src/components/activity/ActivityListItem.jsx
@@ -1,60 +1,104 @@
-import { Grid, Typography ,Container, Stack} from '@mui/material';
-// import { formatDistance, parseISO } from 'date-fns';
-import Avatar from '../avatar/Avatar';
-import Link from '../link/Link';
-import styles from './ActivityListItem.module.scss';
-import formatDistance from 'date-fns/formatDistance'
-import parseISO from 'date-fns/parseISO'
+import styles from "./ActivityListItem.module.scss";
  
-
-const bandera={
-    "created_at": Date,
-    "user": {
-       "avatar": {
-          "url": '/images/nft.jpg'
-       },
-       "verified": true,
-       "name":'Cupcat nft'
-    },
-    "nft":{
-       "name": 'dog bone',
-       "owner": {
-          "username": "antonio banderas",
-           "avatar": {
-              "url": '/images/avatar.png'
-           },
-          "verified": true
-       }
-    },
-    "type": "buy"
-   }
-   
-  let currentDate=new Date();
+import { formatDistance, parseISO } from "date-fns";
  
-export default function ActivityListItem({user ,created_at ,nft ,type="like"}) {
-     created_at='+02023065';
-    let dateInTime=parseISO(created_at, { additionalDigits: 1 })
-  
-  
-   const timeDistanceCalc=formatDistance(currentDate,dateInTime,{addSuffix:true,includeSeconds:true})
-    return (
-               <div> 
-               <Stack className={styles["list-item-stack"]} direction="row" spacing={2}>
-
-               <Avatar url={user.avatar.url} verified={user.verified} />
-
-               <Stack className={styles["info-stack"]} direction="column"> 
-
-                     <p className={styles["info-par"]}> {user} {type=='like'?"liked":(type=="buy"?"bought":null)}  <Link variant="text"  href='/' >{user}</Link> by  <Link variant="text" href='/'>{nft}</Link></p>  
-                     <p className={styles["info-par"]}> { formatDistance(currentDate,dateInTime,{addSuffix:true,includeSeconds:true}) }</p>
-               </Stack>
-               </Stack>
-        </div>
-    );
+import Avatar from "../avatar/Avatar";
+import Link from "../link/Link";
+import Stack from "@mui/material/Stack";
+ 
+export default function ActivityListItem({
+  user,
+  created_at,
+  nft,
+  type = "like",
+}) {
+  const action = () => {
+    if (type === "like") {
+      return "liked";
+    } else if (type === "buy") {
+      return "bought";
+    }
+  };
+  return (
+    <div>
+      <Stack className={styles["list-item-stack"]} direction="row" spacing={2}>
+        <Avatar url={user.avatar.url} verified={user.verified} />
+        <Stack className={styles["info-stack"]} direction="column">
+          <p className={styles["info-par"]}>
+            {`${user.name} ${action()}`}{" "}
+            <Link href={nft.owner.avatar.url}>"{nft.name}"</Link> by{" "}
+            <Link href={user.avatar.url}>{nft.owner.username}</Link>
+          </p>
+          <p className={styles["info-par"]}>
+            {formatDistance(new Date(parseISO(created_at)), new Date(), {
+              addSuffix: true,
+            })}
+          </p>
+        </Stack>
+      </Stack>
+    </div>
+  );
 }
 
 
- {/* <Grid container wrap="nowrap"  alignItems="center" justifyContent="flex-start" > */}
+
+// import { Grid, Typography ,Container, Stack} from '@mui/material';
+// // import { formatDistance, parseISO } from 'date-fns';
+// import Avatar from '../avatar/Avatar';
+// import Link from '../link/Link';
+// import styles from './ActivityListItem.module.scss';
+// import formatDistance from 'date-fns/formatDistance'
+// import parseISO from 'date-fns/parseISO'
+ 
+
+// const bandera={
+//     "created_at": Date,
+//     "user": {
+//        "avatar": {
+//           "url": '/images/nft.jpg'
+//        },
+//        "verified": true,
+//        "name":'Cupcat nft'
+//     },
+//     "nft":{
+//        "name": 'dog bone',
+//        "owner": {
+//           "username": "antonio banderas",
+//            "avatar": {
+//               "url": '/images/avatar.png'
+//            },
+//           "verified": true
+//        }
+//     },
+//     "type": "buy"
+//    }
+   
+//   let currentDate=new Date();
+ 
+// export default function ActivityListItem({user ,created_at ,nft ,type="like"}) {
+//      created_at='+02023065';
+//     let dateInTime=parseISO(created_at, { additionalDigits: 1 })
+  
+  
+//    const timeDistanceCalc=formatDistance(currentDate,dateInTime,{addSuffix:true,includeSeconds:true})
+//     return (
+//                <div> 
+//                <Stack className={styles["list-item-stack"]} direction="row" spacing={2}>
+
+//                <Avatar url={user.avatar.url} verified={user.verified} />
+
+//                <Stack className={styles["info-stack"]} direction="column"> 
+
+//                      <p className={styles["info-par"]}> {user} {type=='like'?"liked":(type=="buy"?"bought":null)}  <Link variant="text"  href='/' >{user}</Link> by  <Link variant="text" href='/'>{nft}</Link></p>  
+//                      <p className={styles["info-par"]}> { formatDistance(currentDate,dateInTime,{addSuffix:true,includeSeconds:true}) }</p>
+//                </Stack>
+//                </Stack>
+//         </div>
+//     );
+// }
+
+
+//  {/* <Grid container wrap="nowrap"  alignItems="center" justifyContent="flex-start" > */}
    {/* <Grid item > */}
      {/* </Grid> */}
             {/* <Grid container direction="column"> */}
