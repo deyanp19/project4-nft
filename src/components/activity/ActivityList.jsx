8166fdace5494928f9afc017ff519f0c2574f528
deyan petrov <deyanp19@hotmail.com>
2023-03-05 20:57:32 +0200

    items array

--- src/components/activity/ActivityList.jsx
+++ src/components/activity/ActivityList.jsx
@@ -3,7 +3,7 @@ import styles from './ActivityList.module.scss';
 import ActivityListItem from './ActivityListItem';
 import classNames from 'classnames';
 
-export default function ActivityList({items}){
+export default function ActivityList({items=[]}){
     return (
         <div className={classNames(styles["activity-list"])}>
             <Stack direction="column" spacing={2}   >
