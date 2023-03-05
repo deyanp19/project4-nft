ae5cd1d426b671f197d595f5a2154add2afd07bc
deyan petrov <deyanp19@hotmail.com>
2023-03-05 09:03:29 +0200

    some touch

--- src/components/activity/ActivityListItem.jsx
+++ src/components/activity/ActivityListItem.jsx
@@ -5,7 +5,7 @@ import Link from '../link/Link';
 import styles from './ActivityListItem.module.scss';
 import { format, formatDistance,parseISO, formatRelative, subDays } from 'date-fns';
 
-export default function ActivityListItem({user='flava', created_at='+02014101',nft='dicksmart',type="like"}) {
+export default function ActivityListItem({user='flava', created_at='+020230305',nft='dicksmart',type="like"}) {
    
     
     return (
@@ -14,15 +14,15 @@ export default function ActivityListItem({user='flava', created_at='+02014101',n
         <div  >
             {/* <Grid container wrap="nowrap"  alignItems="center" justifyContent="flex-start" > */}
                 {/* <Grid item > */}
-                    <Avatar size={56} />
+                    <Avatar url={user} />
                 {/* </Grid> */}
             {/* <Grid container direction="column"> */}
                 {/* <Grid item > */}
-                  {user} {type}  <Link href='/' >{user}</Link>
+               <p> {user} {type=='like'?"liked":(type=="buy"?"bought":null)}  <Link variant="text"  href='/' >{user}</Link> by  <Link variant="text" href='/'>{nft}</Link></p>  
                 {/* </Grid> */}
             
            
-                <p> { formatDistance(new Date()-3, new Date(parseISO('+02014101', { additionalDigits: 1 })), {includeSeconds: true,addSuffix: true})}</p>
+                <p> { formatDistance(new Date(), new Date(parseISO(created_at, { additionalDigits: 1 })), {includeSeconds: true,addSuffix: true})}</p>
             {/* </Grid> */}
             {/* </Grid> */}
         </div>
