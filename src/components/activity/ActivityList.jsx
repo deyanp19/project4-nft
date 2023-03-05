fd0dd1de352783652c30332ab6f74dc5b6c28b29
deyan petrov <deyanp19@hotmail.com>
2023-03-05 21:38:26 +0200

    dont know activity list

--- src/components/activity/ActivityList.jsx
+++ src/components/activity/ActivityList.jsx
@@ -5,11 +5,11 @@ import classNames from "classnames";
 
 export default function ActivityList({ items = [] }) {
   return (
-    <div className={classNames(styles["activity-list"])}>
+    <div className={styles["activity-list"]}>
+        
       <Stack direction="column"  justifyContent="center" alignItems="center" spacing={2}>
         {items.map((item, idx) => {
-          return ( <ActivityListItem key={idx} user={item.user}   nft={item.nft}  created_at={item.created_at} type={item.type} />    );
-        })}
+          return ( <ActivityListItem key={idx} user={item.user}   nft={item.nft}  created_at={item.created_at} type={item.type} />  ); })}
       </Stack>
     </div>
   );
