f25b41730fe610a953e2759651b43bbc4286a914
deyan petrov <deyanp19@hotmail.com>
2023-03-05 07:33:58 +0200

    no grid activity

--- src/components/activity/ActivityListItem.jsx
+++ src/components/activity/ActivityListItem.jsx
@@ -12,19 +12,19 @@ export default function ActivityListItem({user='flava', created_at='+02014101',n
 
         
         <div className={styles.activity} >
-            <Grid container wrap="nowrap"  alignItems="center" justifyContent="flex-start" >
-                <Grid item >
+            {/* <Grid container wrap="nowrap"  alignItems="center" justifyContent="flex-start" > */}
+                {/* <Grid item > */}
                     <Avatar size={56} />
-                </Grid>
-            <Grid container direction="column">
-                <Grid item >
+                {/* </Grid> */}
+            {/* <Grid container direction="column"> */}
+                {/* <Grid item > */}
                   {user} {type}  <Link href='/' >{user}</Link>
-                </Grid>
+                {/* </Grid> */}
             
            
                 <Typography> { formatDistance(new Date()-3, new Date(parseISO('+02014101', { additionalDigits: 1 })), {includeSeconds: true,addSuffix: true})}</Typography>
-            </Grid>
-            </Grid>
+            {/* </Grid> */}
+            {/* </Grid> */}
         </div>
     );
 }
