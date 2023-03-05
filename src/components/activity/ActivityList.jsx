0f902b0d0fa60f75df486ef23b1325756f43034f
deyan petrov <deyanp19@hotmail.com>
2023-03-05 12:13:51 +0200

    activity

--- src/components/activity/ActivityList.jsx
+++ src/components/activity/ActivityList.jsx
@@ -1,9 +1,13 @@
+import { Stack } from '@mui/material';
 import styles from './ActivityList.module.scss';
+import ActivityListItem from './ActivityListItem';
 
-export default function ActivityList(){
+export default function ActivityList({items}){
     return (
-        <div>
-            
+        <div className={styles["activity-list"]}>
+            <Stack spacing={3} >
+            {items.map((item)=><Item><ActivityListItem /> </Item>)}
+            </Stack>
         </div>
     );
 }
--- src/components/activity/ActivityListItem.jsx
+++ src/components/activity/ActivityListItem.jsx
@@ -3,28 +3,56 @@ import { Grid, Typography } from '@mui/material';
 import Avatar from '../avatar/Avatar';
 import Link from '../link/Link';
 import styles from './ActivityListItem.module.scss';
-import { format, formatDistance,parseISO, formatRelative, subDays } from 'date-fns';
+import formatDistance from 'date-fns/formatDistance'
+import parseISO from 'date-fns/parseISO'
 
-export default function ActivityListItem({user='flava', created_at='+020230305',nft='dicksmart',type="like"}) {
+const bandera={
+    "created_at": Date,
+    "user": {
+       "avatar": {
+          "url": '/images/nft.jpg'
+       },
+       "verified": true,
+       "name":'Cupcat nft'
+    },
+    "nft":{
+       "name": 'dog bone',
+       "owner": {
+          "username": "antonio banderas",
+           "avatar": {
+              "url": '/images/avatar.png'
+           },
+          "verified": true
+       }
+    },
+    "type": "buy"
+   }
    
-    
-    return (
 
-        
-        <div  >
-            {/* <Grid container wrap="nowrap"  alignItems="center" justifyContent="flex-start" > */}
-                {/* <Grid item > */}
-                    <Avatar url={user} />
-                {/* </Grid> */}
-            {/* <Grid container direction="column"> */}
-                {/* <Grid item > */}
-               <p> {user} {type=='like'?"liked":(type=="buy"?"bought":null)}  <Link variant="text"  href='/' >{user}</Link> by  <Link variant="text" href='/'>{nft}</Link></p>  
-                {/* </Grid> */}
-            
-           
-                <p> { formatDistance(new Date(), new Date(parseISO(created_at, { additionalDigits: 1 })), {includeSeconds: true,addSuffix: true})}</p>
-            {/* </Grid> */}
-            {/* </Grid> */}
+export default function ActivityListItem({user='flava', created_at='+02023065',nft='dicksmart',type="like"}) {
+    let dateInTime=parseISO(created_at, { additionalDigits: 1 })
+    let currentDate=new Date()
+   console.log()
+    
+    return (<div  >
+            <Avatar url={user} />
+            <p> {user} {type=='like'?"liked":(type=="buy"?"bought":null)}  <Link variant="text"  href='/' >{user}</Link> by  <Link variant="text" href='/'>{nft}</Link></p>  
+            <p> { formatDistance(currentDate,dateInTime,{addSuffix:true,includeSeconds:true})}</p>
+         
         </div>
     );
 }
+
+
+ {/* <Grid container wrap="nowrap"  alignItems="center" justifyContent="flex-start" > */}
+   {/* <Grid item > */}
+     {/* </Grid> */}
+            {/* <Grid container direction="column"> */}
+
+     {/* <Grid item > */}
+  {/* </Grid> */}
+   {/* </Grid> */}
+            {/* </Grid> */}
+
+
+
