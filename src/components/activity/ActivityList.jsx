3bd3c5704c0139703702f09a5d03bfc2abf24ac5
deyan petrov <deyanp19@hotmail.com>
2023-03-05 20:49:43 +0200

    added container class activity list exist items not;

--- src/components/activity/ActivityList.jsx
+++ src/components/activity/ActivityList.jsx
@@ -1,13 +1,13 @@
-import { Stack } from '@mui/material';
+import { Container, Stack } from "@mui/material";
 import styles from './ActivityList.module.scss';
 import ActivityListItem from './ActivityListItem';
 import classNames from 'classnames';
 
-export default function ActivityList({items=[]}){
+export default function ActivityList({items}){
     return (
         <div className={classNames(styles["activity-list"])}>
-            <Stack spacing={2} alignItems='center' direction="column" >
-            {items.map((item)=> (<ActivityListItem key={item.name+item.created_at}
+            <Stack direction="column" spacing={2}   >
+            {items && items.map((item)=> (<ActivityListItem key={item.name+item.created_at}
                   user={item.user} 
                   created_at={item.created_at} 
                   nft={item.nft} 
