7ebffffdc69dfe835676634079baa44f2f06021b
deyan petrov <deyanp19@hotmail.com>
2023-03-05 21:07:07 +0200

    no white spaces

--- src/components/activity/ActivityList.jsx
+++ src/components/activity/ActivityList.jsx
@@ -6,12 +6,8 @@ import classNames from 'classnames';
 export default function ActivityList({items=[]}){
     return (
         <div className={classNames(styles["activity-list"])}>
-            <Stack direction="column" spacing={2}   >
-            {items && items.map((item)=> (<ActivityListItem key={item.name+item.created_at}
-                  user={item.user} 
-                  created_at={item.created_at} 
-                  nft={item.nft} 
-                  type={item.type} />) )}
+            <Stack direction="column" spacing={2}  alignItems="center" >
+            {items && items.map((item)=> (<ActivityListItem key={item.name+item.created_at} user={item.user}    created_at={item.created_at}  nft={item.nft} type={item.type} />) )}
             </Stack>
         </div>
     );
