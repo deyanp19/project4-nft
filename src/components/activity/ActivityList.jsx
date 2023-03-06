15220050ecbee20d35b1ae180283d937c2ec5e6f
DEYAN PETROV <deyanpetrov@DEYANs-MacBook-Pro.local>
2023-03-06 23:34:05 +0200

    no container

--- src/components/activity/ActivityList.jsx
+++ src/components/activity/ActivityList.jsx
@@ -8,13 +8,13 @@ import classNames from "classnames";
   export default function ActivityList({ items = [] }) {
     return (
       <div className={styles["activity-list"]}>
-        <Container>
+       
           <Stack direction={"column"} spacing={2}>
             {items.map((item, i) => {
               return <ActivityListItem key={i} {...item} />;
             })}
           </Stack>
-        </Container>
+        
       </div>
     );
   }
