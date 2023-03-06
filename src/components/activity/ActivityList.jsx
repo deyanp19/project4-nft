e904369488530d08877906a571394bb7be112543
DEYAN PETROV <deyanpetrov@DEYANs-MacBook-Pro.local>
2023-03-07 01:27:23 +0200

    class names

--- src/components/activity/ActivityList.jsx
+++ src/components/activity/ActivityList.jsx
@@ -2,10 +2,11 @@ import styles from "./ActivityList.module.scss";
 import ActivityListItem from "./ActivityListItem";
 import { Container } from "@mui/material";
 import Stack from '@mui/material/Stack';
+import classNames from 'classnames'
 
 export default function ActivityList({ items = [] }) {
   return (
-    <div className={styles["activity-list"]}>
+    <div className={classNames(styles["activity-list"])}>
         <Stack direction={"column"} spacing={2}>
           {items.map((item, i) => {
             return <ActivityListItem key={i} user={item.user}   nft={item.nft}  created_at={item.created_at} type={item.type} />;
