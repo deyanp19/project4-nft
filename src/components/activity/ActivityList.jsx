45176a0ad5521a77ec84f31e4827a3c8b0a2767a
DEYAN PETROV <deyanpetrov@DEYANs-MacBook-Pro.local>
2023-03-07 20:30:32 +0200

    reinstare activity

--- src/components/activity/ActivityList.jsx
+++ src/components/activity/ActivityList.jsx
@@ -9,7 +9,7 @@ export default function ActivityList({ items = [] }) {
     <div className={classNames(styles["activity-list"])}>
         <Stack  direction={"column"} spacing={2}>
           {items.map((item, i) => {
-            return <ActivityListItem key={i} user={item.user}   nft={item.nft}  created_at={item.created_at} type={item.type} />;
+            return (<ActivityListItem key={i} user={item.user}   nft={item.nft}  created_at={item.created_at} type={item.type} />);
           })}
         </Stack>
    
