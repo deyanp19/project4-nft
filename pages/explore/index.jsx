7d0410c9f54e3158645a7c0e9271f3c4cb75387e
DEYAN PETROV <deyanpetrov@DEYANs-MacBook-Pro.local>
2023-03-07 21:47:29 +0200

    using ai

---
+++ pages/explore/index.jsx
@@ -0,0 +1,35 @@
+import * as React from "react";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
+import Link from "../../src/components/link/Link";
+import Header from "../../src/components/header/Header";
+import Footer from "../../src/components/footer/Footer";
+import Hero from "../../src/components/hero/Hero"
+import ActivityList from "../../src/components/activity/ActivityList";
+import dataActivity from "../../data/activity.json"
+import { useEffect, useState } from "react";
+import ExploreTitle from "../../src/components/explore/ExploreTitle";
+import ExploreFilters from "../../src/components/explore/ExploreFilters";
+
+export default function About() {
+
+    const [activity,setActivity] =useState([])
+
+    useEffect(()=>{
+        setActivity(dataActivity);
+    },[]);
+
+  return (
+    <div>
+
+      <Header/>
+    <ExploreTitle />
+    <ExploreFilters />
+        <Footer />
+
+    </div>
+    
+  );
+}
+
--- src/components/activity/ActivityList.jsx
+++ src/components/activity/ActivityList.jsx
@@ -1,7 +1,7 @@
 import styles from "./ActivityList.module.scss";
 import ActivityListItem from "./ActivityListItem";
 import { Container } from "@mui/material";
-import Stack from '@mui/material/Stack';
+mport { Stack } from '@mui/material';
 import classNames from 'classnames'
 
 export default function ActivityList({ items  }) {
@@ -12,11 +12,8 @@ export default function ActivityList({ items  }) {
     textAlign: 'center',
     color: theme.palette.text.secondary,
   }));
-  return (
-    <div className={classNames(styles["activity-list"])}>
-        <Stack  direction={"column"} spacing={2}>
-          {items.map((item, i) => {
-            return ( <Item><ActivityListItem key={i} user={item.user}   nft={item.nft}  created_at={item.created_at} type={item.type} /></Item>);
+  return ( <div className={`activity-list ${styles.activityList}`}> <Stack direction="column" spacing={2}>  {items.map((item, i) => {
+            return ( <ActivityListItem key={i} user={item.user}   nft={item.nft}  created_at={item.created_at} type={item.type} />);
           })}
         </Stack>
    
