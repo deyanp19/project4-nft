c53debe1b6dc67544a46ee94f1946a9c9cac55ee
DEYAN PETROV <deyanpetrov@DEYANs-MacBook-Pro.local>
2023-03-07 21:30:48 +0200

    stack wiht item

--- src/components/activity/ActivityList.jsx
+++ src/components/activity/ActivityList.jsx
@@ -5,11 +5,18 @@ import Stack from '@mui/material/Stack';
 import classNames from 'classnames'
 
 export default function ActivityList({ items  }) {
+  const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  }));
   return (
     <div className={classNames(styles["activity-list"])}>
         <Stack  direction={"column"} spacing={2}>
           {items.map((item, i) => {
-            return (<ActivityListItem key={i} user={item.user}   nft={item.nft}  created_at={item.created_at} type={item.type} />);
+            return ( <Item><ActivityListItem key={i} user={item.user}   nft={item.nft}  created_at={item.created_at} type={item.type} /></Item>);
           })}
         </Stack>
    
