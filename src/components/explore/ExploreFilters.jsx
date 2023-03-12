dd577f7dac71098173b19a4edb20b8ccd9ba36fa
DEYAN PETROV <deyanpetrov@DEYANs-MacBook-Pro.local>
2023-03-13 00:16:13 +0200

    ready activity

--- src/components/explore/ExploreFilters.jsx
+++ src/components/explore/ExploreFilters.jsx
@@ -1,4 +1,4 @@
-import { Container, FormControl, InputAdornment, MenuItem, TextField, Typography,Grid,Select } from '@mui/material';
+import { Container, FormControl, InputAdornment, MenuItem, TextField, Typography,Grid,Select, InputLabel } from '@mui/material';
 import classNames from 'classnames';
 import styles from './ExploreFilters.module.scss';
 import {Search} from '@mui/icons-material';
@@ -9,11 +9,14 @@ export default function ExploreFilters({filters}) {
             <Container>
                 <Grid container direction="column" >
                      <FormControl sx={{m:1}} size="small" fullWidth>
+                     <InputLabel htmlFor="price">Price range</InputLabel>
+
                          <Select color="primary" labelId="sort-order" id="sort-order" variant="outlined" value={1} label={filters?  'sdf' :'none' }>
-                             {/* {filters.sort.map((x)=><MenuItem key={x.value} value={x.value}>{x.label}</MenuItem>)} */}
+                              {filters.sort.map((x)=><MenuItem key={x.value} value={x.value}>{x.label}</MenuItem>)}  
                          </Select>
                      </FormControl>
                      <FormControl sx={{m:1}} size="small" fullWidth>
+                     <InputLabel htmlFor="price">Sort by</InputLabel>
                      <Select 
                                 labelId="price"
                                 id="price"
@@ -26,6 +29,7 @@ export default function ExploreFilters({filters}) {
                                </Select>
                      </FormControl>
                      <FormControl sx={{padding:"4"}} >  
+
                                 <TextField variant="standard" InputProps={{  startAdornment: (  <InputAdornment position='start' > <Search sx={{color:'white'}}  /> </InputAdornment> ),   disableUnderline:true}} className={classNames(styles.backgroundInputSearch)}  fullWidth    color='secondary'  />
                             </FormControl>  
                 </Grid>
