dc1176dfb9556ebbfc6f30025385a040868ab2c9
deyan petrov <deyanp19@hotmail.com>
2023-03-14 17:25:24 +0200

    second use of stack

--- src/components/explore/ExploreFilters.jsx
+++ src/components/explore/ExploreFilters.jsx
@@ -1,10 +1,18 @@
-import { Container, FormControl, InputAdornment, MenuItem, TextField, Typography,Grid,Select, InputLabel, Stack } from '@mui/material';
-import classNames from 'classnames';
-import styles from './ExploreFilters.module.scss';
-import {Search} from '@mui/icons-material';
+import styles from "./ExploreFilters.module.scss";
+import {
+  FormControl,
+  InputAdornment,
+  InputLabel,
+  MenuItem,
+  Select,
+  Stack,
+  TextField,
+} from "@mui/material";
+
 import SearchIcon from "@mui/icons-material/Search";
 
 export default function ExploreFilters({filters}) {
+    console.log(filters);
     return (
         <div className={styles["explore-filters"]}>
             {/* <Container> */}
@@ -15,22 +23,22 @@ export default function ExploreFilters({filters}) {
                      <InputLabel shrink htmlFor="price">Price range</InputLabel>
 
                          <Select color="primary" labelId="sort-order" id="sort-order" variant="outlined" value={1}  >
-                            {/* <Stack direction="column" spacing={2}> */}
-                              { filters.sort.map((x)=>{
-                                  return (<MenuItem key={x.value} value={x.value}>{x.label}</MenuItem>)})}   
-                            {/* </Stack> */}
+                             <Stack direction="column" spacing={2}> 
+                               { filters.sort.map((x)=>{
+                                  return (<MenuItem key={x.value} value={x.value}>{x.label}</MenuItem>)})}    
+                             </Stack> 
                          </Select>
                      </FormControl>
                      <FormControl sx={{m:1}} size="small" fullWidth>
                      <InputLabel htmlFor="price">Sort by</InputLabel>
-                     <Select labelId="price" id="price" value={3}  color='primary' className={classNames(styles.selectBox)}  >
+                     <Select labelId="price" id="price" value={3}  color='primary' className={ styles.selectBox}  >
                                     {/* <Stack direction="column" spacing={2}> */}
 
-                                    {filters.price.map((timon)=><MenuItem key={timon.value} value={timon.value}>{timon.label}</MenuItem>)}    
+                                    {/* {filters.price.map((timon)=><MenuItem key={timon.value} value={timon.value}>{timon.label}</MenuItem>)}     */}
                                     {/* </Stack> */}
                                </Select>
                      </FormControl>
-                      <TextField  InputProps={{  startAdornment: (  <InputAdornment position='start' > <SearchIcon className={styles["search-icon"]}  /> </InputAdornment> ),   disableUnderline:true, sx:{ width:"340px"}}} className={classNames(styles.backgroundInputSearch)}       variant="standard" placeholder='' />
+                      <TextField  InputProps={{  startAdornment: (  <InputAdornment position='start' > <SearchIcon className={styles["search-icon"]}  /> </InputAdornment> ),   disableUnderline:true, sx:{ width:"340px"}}} className={styles.backgroundInputSearch}       variant="standard" placeholder='' />
                               
                 </Stack>
              
