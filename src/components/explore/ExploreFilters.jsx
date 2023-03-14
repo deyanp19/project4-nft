c03b47111028e314455999f626edde0a1d29484f
DEYAN PETROV <deyanpetrov@DEYANs-MacBook-Pro.local>
2023-03-14 13:49:46 +0200

    stack applied to filters form controll

--- src/components/explore/ExploreFilters.jsx
+++ src/components/explore/ExploreFilters.jsx
@@ -7,16 +7,16 @@ export default function ExploreFilters({filters}) {
     return (
         <div className={styles["explore-filters"]}>
             <Container>
-                <Stack direction="column" spacing={2}>
-                {/* <Grid container direction="column" > */}
+                
+                <Grid container direction="column" >
 
                      <FormControl sx={{m:1}} size="small" fullWidth>
                      <InputLabel shrink htmlFor="price">Price range</InputLabel>
 
                          <Select color="primary" labelId="sort-order" id="sort-order" variant="outlined" value={1} label={filters?  'sdf' :'none' }>
-                    
-                              {filters.sort.map((x)=><MenuItem key={x.value} value={x.value}>{x.label}</MenuItem>)}  
-                     
+                    <Stack direction="column" spacing={2}>
+                              {/* {filters.sort.map((x)=><MenuItem key={x.value} value={x.value}>{x.label}</MenuItem>)}   */}
+                        </Stack>
                          </Select>
                      </FormControl>
                      <FormControl sx={{m:1}} size="small" fullWidth>
@@ -36,8 +36,8 @@ export default function ExploreFilters({filters}) {
 
                                 <TextField variant="standard" InputProps={{  startAdornment: (  <InputAdornment position='start' > <Search sx={{color:'white'}}  /> </InputAdornment> ),   disableUnderline:true}} className={classNames(styles.backgroundInputSearch)}  fullWidth    color='secondary'  />
                             </FormControl>  
-                {/* </Grid> */}
-                </Stack>
+                </Grid>
+             
             </Container>
         </div>
     );
