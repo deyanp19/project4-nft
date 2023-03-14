2dde941823469997686b42b3274956c06338b353
deyan petrov <deyanp19@hotmail.com>
2023-03-14 13:36:02 +0200

    stack instead of grid

--- src/components/explore/ExploreFilters.jsx
+++ src/components/explore/ExploreFilters.jsx
@@ -7,15 +7,16 @@ export default function ExploreFilters({filters}) {
     return (
         <div className={styles["explore-filters"]}>
             <Container>
-                <Grid container direction="column" >
+                <Stack direction="column" spacing={2}>
+                {/* <Grid container direction="column" > */}
 
                      <FormControl sx={{m:1}} size="small" fullWidth>
                      <InputLabel shrink htmlFor="price">Price range</InputLabel>
 
                          <Select color="primary" labelId="sort-order" id="sort-order" variant="outlined" value={1} label={filters?  'sdf' :'none' }>
-                    <Stack direction="column" spacing={2}>
+                    
                               {filters.sort.map((x)=><MenuItem key={x.value} value={x.value}>{x.label}</MenuItem>)}  
-                     </Stack>
+                     
                          </Select>
                      </FormControl>
                      <FormControl sx={{m:1}} size="small" fullWidth>
@@ -35,7 +36,8 @@ export default function ExploreFilters({filters}) {
 
                                 <TextField variant="standard" InputProps={{  startAdornment: (  <InputAdornment position='start' > <Search sx={{color:'white'}}  /> </InputAdornment> ),   disableUnderline:true}} className={classNames(styles.backgroundInputSearch)}  fullWidth    color='secondary'  />
                             </FormControl>  
-                </Grid>
+                {/* </Grid> */}
+                </Stack>
             </Container>
         </div>
     );
