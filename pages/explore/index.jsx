12239948af6306d93c6558a17bd8f4bdc1a18168
deyan petrov <deyanp19@hotmail.com>
2023-03-16 22:23:18 +0200

    on any change

--- pages/explore/index.jsx
+++ pages/explore/index.jsx
@@ -33,28 +33,29 @@ export default function About() {
       console.log(e.target.value);
       setSortVal(e.target.value);
     }
-    // useEffect(async ()=>{
-    //   async function getExplore(price,sort){
-    //       return await (await fetch(url+'/explore'+`?price=${price}&sort=${sort}`)).json();
 
-    //   }
-    //   async function resolve(callback,price,sort){
-    //         let resolvedData= await callback(price,sort);
-    //         switch (true) {
-    //           case callback.name=='getExplore':
-    //             const {nfts,filters}=resolvedData;
-    //             setFilters(filters);
-    //             setNtfs(nfts);
-    //             break;
+    useEffect(async ()=>{
+      async function getExplore(price,sort){
+          return await (await fetch(url+'/explore'+`?price=${price}&sort=${sort}`)).json();
+
+      }
+      async function resolve(callback,price,sort){
+            let resolvedData= await callback(price,sort);
+            switch (true) {
+              case callback.name=='getExplore':
+                const {nfts,filters}=resolvedData;
+                setFilters(filters);
+                setNtfs(nfts);
+                break;
                
               
-    //           default:
-    //             break;
-    //         }
-    //       }
+              default:
+                break;
+            }
+          }
 
-    //  await  resolve(getExplore,priceVal,sortVal);
-    // },[priceVal,sortVal])
+     await  resolve(getExplore,priceVal,sortVal);
+    },[priceVal,sortVal])
 
     useEffect(async ()=>{
       async function getExplore(sort){
@@ -106,7 +107,7 @@ export default function About() {
     useEffect(()=>{
       async function getExplore(){
         
-          return await (await fetch(url+'/explore'+`?sort=1&price=8`)).json();
+          return await (await fetch(url+'/explore')).json();
         
       }
       async function resolve(callback,price,sort){
